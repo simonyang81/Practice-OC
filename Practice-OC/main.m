33bed87fd2ab40ab7f96aed9568cf95790cc8da1
simonyang81 <simonyang81@gmail.com>
2016-01-19 21:13:19 +0800

    initialization

---
+++ Practice-OC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice-OC
+//
+//  Created by Simon Yang on 1/19/16.
+//  Copyright (c) 2016 Simon Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+
+    return 0;
+}
