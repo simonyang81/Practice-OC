69524b5b45799ed1abc65fe90574c484f677bb8b
simonyang81 <simonyang81@gmail.com>
2016-03-22 23:15:58 +0800

    add desc for category

--- Practice-OC/NSNumber+sm.h
+++ Practice-OC/NSNumber+sm.h
@@ -8,9 +8,14 @@
 /**
  * Objective-C Category
  *
+ * Objective-C 的动态特征允许使用Category为现有的类添加方法, 并且不需要创建子类, 不需要访问原来类的源代码
+ *
+ * 下面的程序为NSNumber增加一个Category, Category接口部分的代码如下:
+ *
  */
 @interface NSNumber (sm)
 
+// 在Category中定义4个方法, 相当于为NSNumber新增了四个自定义的方法
 - (NSNumber *) add:(double) num2;
 - (NSNumber *) substract: (double) num2;
 - (NSNumber *) multiply: (double) num2;
